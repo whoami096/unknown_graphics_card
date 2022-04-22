8e004990566d1b60763a4c4520ed2119cea27b3b
maverick <ahegaolei@163.com>
2022-04-22 21:12:59 +0800

    www.acwing.com

---
+++ 模板/23_单调队列.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+const int N = 1e6+10;
+int a[N], q[N];
+int hh = 0, tt = -1;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for(int i=0;i<n;i++) scanf("%d", &a[i]);
+    for(int i=0;i<n;i++)
+    {
+        if(hh<=tt&&i-k+1>q[hh]) hh++;
+        while(hh<=tt&&a[q[tt]]>=a[i]) tt--;
+        q[++tt] = i;
+        if(i>=k-1) printf("%d ", a[q[hh]]);
+    }
+    putchar(10);
+    hh = 0, tt = -1;
+    for(int i=0;i<n;i++)
+    {
+        if(hh<=tt&&i-k+1>q[hh]) hh++;
+        while(hh<=tt&&a[q[tt]]<=a[i]) tt--;
+        q[++tt] = i;
+        if(i>=k-1) printf("%d ", a[q[hh]]);
+    }
+    putchar(10);
+    return 0;
+}
