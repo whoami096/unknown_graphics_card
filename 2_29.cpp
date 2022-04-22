9a80070234e5cf3a2d64c440d6867b4edb8f9028
maverick <ahegaolei@163.com>
2022-04-22 19:47:53 +0800

    Delete 2_29.cpp

--- 2_29.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    printf("integer\tsquare\tcube\n");
-    for(int i=0;i<11;i++)
-    printf("%d\t%d\t%d\n", i, i*i, i*i*i);
-    return 0;
-}
