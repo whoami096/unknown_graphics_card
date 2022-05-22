93c8476155e2a2f40fc9770f4f94614a6524aa05
maverick <ahegaolei@163.com>
2022-05-22 10:55:55 +0800

    手动实现一个vector
    功能还不是很全，部分函数功能比较简易，先这样吧

---
+++ myvector.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template <class T>
+class myvector
+{
+private:
+    bool check(int tmp)
+    {
+        if(tmp < 32) return false;
+        while(!(tmp&1)) tmp >>= 1;
+        tmp >>= 1;
+        if(tmp) return false;
+        else return true; 
+    }
+public:
+    T * a = new T[32];
+    T operator[](int k)
+    {return *(a+k);}
+    int cnt = 0;
+    int size() {return cnt;}
+    void push_back(T k)
+    {
+        if(this->check(cnt))
+        {
+            T *tmp = new T[(cnt)*2];
+            memcpy(tmp, a, sizeof(T)*cnt);
+            delete []a;
+            a = tmp;
+        }
+        *(a + cnt ++) = k;
+    }
+    void pop_back() { cnt--; }
+
+    int back() { return *(a+cnt-1); }
+
+    void clear() { cnt = 0;}
+    T *begin(){return a;}
+    T *end(){return a+cnt;}
+    void erase(T *tmp1, T *tmp2) { cnt = tmp1 - a; }
+};
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    myvector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        a.push_back(n - i);
+    }
+
+    
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << ' ';
+    }
+    
+    return 0;
+}
